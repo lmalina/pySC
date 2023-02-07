f0dd97b2b0a1083e6bf93adf7eb9d10c034c1dd3
lmalina <lukas.malina@pm.me>
2023-02-07 14:06:50 +0100

    Basic package structure (#1)

---
+++ pySC/__init__.py
@@ -0,0 +1,16 @@
+"""
+pySC package
+~~~~~~~~~~~~~~~~
+
+pySC
+
+"""
+
+__title__ = "pySC"
+__description__ = "Python version of Simulated Commissioning toolkit for synchrotrons (https://github.com/ThorstenHellert/SC) "
+__url__ = "https://github.com/lmalina/pySC"
+__version__ = "0.1.0"
+__author__ = "lmalina"
+__author_email__ = "lukas.malina@desy.de"
+
+__all__ = [__version__]
