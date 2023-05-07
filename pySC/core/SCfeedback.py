d9baa78fe250eccfbbdaf8efdacbdb6738cf1d5c
ThorstenHellert <thellert@lbl.gov>
2023-05-07 12:21:54 -0700

    SCfeedbackRun now works

--- pySC/core/SCfeedback.py
+++ pySC/core/SCfeedback.py
@@ -103,17 +103,17 @@ def SCfeedbackStitch(SC, Mplus, R0=None, CMords=None, BPMords=None, nBPMs=4, max
     raise RuntimeError('SCfeedbackStitch: FAIL Reached maxsteps')
 
 
-def SCfeedbackRun(SC, Mplus, R0=None, CMords=None, BPMords=None, eps=1e-5, target=0, maxsteps=30, scaleDisp=0, weight=None, do_plot=False):
+def SCfeedbackRun(SC, Mplus, R0=None, CMords=None, BPMords=None, eps=1e-5, target=0, maxsteps=30, scaleDisp=0, weight=None):
     if weight is None:
         weight = np.ones((Mplus.shape[1], 1))
     BPMords, CMords, R0 = _check_ords(SC, Mplus, R0, BPMords, CMords, "Run")
     transmission_history = None
     rms_orbit_history = None
 
+    B, transmission_history, rms_orbit_history = _bpm_reading_and_logging(SC, BPMords=BPMords,ind_history=transmission_history, orb_history=rms_orbit_history, do_plot=SC.plot) # Inject ...
+ 
     for steps in range(maxsteps):
-        B, transmission_history, rms_orbit_history = _bpm_reading_and_logging(SC, BPMords=BPMords,ind_history=transmission_history, orb_history=rms_orbit_history, do_plot=SC.plot) # Inject ...
-
-
+    
 
         R = B[:,:].reshape(R0.shape)
         R[np.isnan(R)] = 0
@@ -123,18 +123,20 @@ def SCfeedbackRun(SC, Mplus, R0=None, CMords=None, BPMords=None, eps=1e-5, targe
             dphi = dphi[:-1]  # TODO the last setpoint is cavity frequency
         SC, _ = SCsetCMs2SetPoints(SC, CMords[0], -dphi[:len(CMords[0])], skewness=False, method="add")
         SC, _ = SCsetCMs2SetPoints(SC, CMords[1], -dphi[len(CMords[0]):], skewness=True, method="add")
+        
+        B, transmission_history, rms_orbit_history = _bpm_reading_and_logging(SC, BPMords=BPMords,ind_history=transmission_history, orb_history=rms_orbit_history, do_plot=SC.plot) # Inject ...
 
-    if np.any(np.isnan(B[0, :])):
-        raise RuntimeError('SCfeedbackRun: FAIL (lost transmission)')
-    if max(rms_orbit_history[-1]) < target and _is_stable_or_converged(min(10, maxsteps), eps, rms_orbit_history):
-        LOGGER.debug("SCfeedbackRun: Success (target reached)")
-        return SC
-    if _is_stable_or_converged(3, eps, rms_orbit_history):
-        LOGGER.debug(f"SCfeedbackRun: Success (converged after {steps:d} steps)")
-        return SC
-    if _is_stable_or_converged(min(10, maxsteps), eps, rms_orbit_history) or maxsteps == 1:
-        LOGGER.debug("SCfeedbackRun: Success (maxsteps reached)")
-        return SC
+        if np.any(np.isnan(B[0, :])):
+            raise RuntimeError('SCfeedbackRun: FAIL (lost transmission)')
+        if max(rms_orbit_history[-1]) < target and _is_stable_or_converged(min(10, maxsteps), eps, rms_orbit_history):
+            LOGGER.debug("SCfeedbackRun: Success (target reached)")
+            return SC
+        if _is_stable_or_converged(3, eps, rms_orbit_history):
+            LOGGER.debug(f"SCfeedbackRun: Success (converged after {steps:d} steps)")
+            return SC
+        if _is_stable_or_converged(min(10, maxsteps), eps, rms_orbit_history) or maxsteps == 1:
+            LOGGER.debug("SCfeedbackRun: Success (maxsteps reached)")
+            return SC
     raise RuntimeError("SCfeedbackRun: FAIL (maxsteps reached, unstable)")
 
 
