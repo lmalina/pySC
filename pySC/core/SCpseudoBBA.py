d17aa236886f58938462c44059311df05c0cf415
ThorstenHellert <thellert@lbl.gov>
2023-05-07 10:33:43 -0700

    SCpseudoBBA now works

--- pySC/core/SCpseudoBBA.py
+++ pySC/core/SCpseudoBBA.py
@@ -7,11 +7,11 @@ def SCpseudoBBA(SC, BPMords, MagOrds, postBBAoffset, sigma=2):
     # TODO this looks fishy ... assumes BPMs attached to quads?
     #  at the same time two separate 2D arrays?
     if len(postBBAoffset) == 1:
-        postBBAoffset = np.repeat(postBBAoffset, 2 * np.size(BPMords, 1))
-    for nBPM in range(np.size(BPMords, 1)):
+        postBBAoffset = np.tile(postBBAoffset, (2,np.size(BPMords, axis=1)))
+    for nBPM in range(np.size(BPMords, axis=1)):
         for nDim in range(2):
-            SC.RING[BPMords[nBPM, nDim]].Offset[nDim] = (SC.RING[MagOrds[nBPM, nDim]].MagnetOffset[nDim]
-                                                         + SC.RING[MagOrds[nBPM, nDim]].SupportOffset[nDim]
-                                                         - SC.RING[BPMords[nBPM, nDim]].SupportOffset[nDim]
-                                                         + postBBAoffset[nDim, nBPM] * SCrandnc(sigma))
+            SC.RING[BPMords[nDim][nBPM]].Offset[nDim] = (SC.RING[MagOrds[nDim][nBPM]].MagnetOffset[nDim]
+                                                         + SC.RING[MagOrds[nDim][nBPM]].SupportOffset[nDim]
+                                                         - SC.RING[BPMords[nDim][nBPM]].SupportOffset[nDim]
+                                                         + postBBAoffset[nDim][nBPM] * SCrandnc(sigma))
     return SC
