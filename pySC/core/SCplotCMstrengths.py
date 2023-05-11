50f85b96724d1349e08b8bb053a1a4bf05a24274
malina <lukas.malina@pm.me>
2023-05-12 01:40:25 +0200

    Little cleanup SCplotCMstrengths

--- pySC/core/SCplotCMstrengths.py
+++ pySC/core/SCplotCMstrengths.py
@@ -4,44 +4,34 @@ from pySC.at_wrapper import findspos
 
 
 def SCplotCMstrengths(SC):
-    fieldNames = ['SetPointB','SetPointA']; # {hor,ver}
-    plt.figure(86)
-    CMs=[]
-    CMval=[[],[]]
+    f, ax = plt.subplots(nrows=2, num=86)
+    CMs = []
+    CMval = [[], []]
 
     for nDim in range(2):
-        CMs.append(findspos(SC.RING,SC.ORD.CM[nDim]))
-        nCM=1;
+        CMs.append(findspos(SC.RING, SC.ORD.CM[nDim]))
         for ord in SC.ORD.CM[nDim]:
             if SC.RING[ord].PassMethod == 'CorrectorPass':
-                # CMval[nDim][nCM] = SC.RING[ord].KickAngle[nDim];
-                CMval[nDim].append(1E6*SC.RING[ord].KickAngle[nDim])
+                CMval[nDim].append(1E6 * SC.RING[ord].KickAngle[nDim])
             else:
-                # CMval[nDim][nCM] = SC.RING[ord][fieldNames[nDim]][1] * SC.RING[ord].Length;
-                CMval[nDim].append(1E6*(SC.RING[ord].SetPointA[0] if nDim else SC.RING[ord].SetPointB[0]) * SC.RING[ord].Length)
-            nCM=nCM+1
-    
-    plt.subplot(2,1,1)
-    for nDim in range(2):
-        plt.bar(CMs[nDim],CMval[nDim])
-    #plt.ylabel('CM strengh [$\mu$rad]');plt.xlabel('s [m]');
-    # plt.title([sprintf('$\\Theta_x=%.0f\\mu$rad rms,            ', 1E6*np.sqrt(np.mean(CMval[1]**2))),...
-    #     sprintf('$\\Theta_y=%.0f\\mu$rad rms', 1E6*np.sqrt(np.mean(CMval[2]**2)))],'Interpreter','latex')
-    # plt.gca().box('on')
-    plt.subplot(2,1,2)
-    for nDim in range(2):
+                CMval[nDim].append(1E6 * SC.RING[ord].Length *
+                                   (SC.RING[ord].SetPointA[0] if nDim else SC.RING[ord].SetPointB[0]))
+        ax[0].bar(CMs[nDim], CMval[nDim])
         count, bins_count = np.histogram(CMval[nDim], bins=len(CMval[nDim]))
         cdf = np.cumsum(count / sum(count))
-        plt.plot(bins_count[1:], cdf)
+        ax[1].plot(bins_count[1:], cdf)
 
-    
-        # plt.xlabel('CM strengh [$\mu$rad]');plt.ylabel('CDF');
-        # plt.legend({'Horizontal','Vertical'})
-        # plt.gca().ylim([0 1]).box('on')
-        # plt.set(findall(plt.gcf, '-property', 'TickLabelInterpreter'), 'TickLabelInterpreter', 'latex');
-        # plt.set(findall(plt.gcf, '-property', 'Interpreter'), 'Interpreter', 'latex');
-        # plt.set(findall(plt.gcf, '-property', 'FontSize'), 'FontSize', 18);
-        # plt.gcf().color('w');
+    ax[0].set_ylabel(r'CM strength [$\mu$rad]')
+    ax[0].set_xlabel('s [m]')
+    # ax[0].set_title(f'$\\Theta_x={1E6*np.sqrt(np.mean(CMval[1]**2)):.0f}\\mu$rad rms,            '
+    #                 f'\n$\\Theta_y={1E6*np.sqrt(np.mean(CMval[2]**2)):.0f}\\mu$rad rms',
+    #                 Interpreter='latex')
+    ax[1].set_xlabel(r'CM strength [$\mu$rad]')
+    ax[1].set_ylabel('CDF')
+    ax[1].legend(['Horizontal', 'Vertical'])
+    ax[1].set_ylim([0, 1])
+    # plt.set(findall(plt.gcf, '-property', 'TickLabelInterpreter'), 'TickLabelInterpreter', 'latex')
+    # plt.set(findall(plt.gcf, '-property', 'Interpreter'), 'Interpreter', 'latex')
+    # plt.set(findall(plt.gcf, '-property', 'FontSize'), 'FontSize', 18)
+    # plt.gcf().color('w')
     plt.show()
-# End
- 
