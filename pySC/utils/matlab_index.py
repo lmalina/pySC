e20ed614b8c48beb013f3c9cdce1a23640aa8d2b
malina <lukas.malina@pm.me>
2023-05-24 20:15:57 +0200

    Fixing(commenting out) BBA import and reformatting

--- pySC/utils/matlab_index.py
+++ pySC/utils/matlab_index.py
@@ -8,38 +8,39 @@ The functions are named identically to SC, and they call their pySC equivalents 
 
 """
 
-from at import Lattice
-import numpy as np
-from numpy import ndarray
 from typing import Tuple
-from pySC.correction.orbit_trajectory import SCfeedbackFirstTurn as first_turn, SCfeedbackStitch as stitch, \
-    SCfeedbackRun as frun, SCfeedbackBalance as fbalance, SCpseudoBBA as pseudo_bba
-from pySC.correction.injection_fit import SCfitInjectionZ as fit_injection
+
+import numpy as np
+from at import Lattice
+from numpy import ndarray
+
 from pySC.core.beam import SCgetBPMreading as bpm_reading, SCgenBunches as generate_bunches
 from pySC.core.beam import SCgetBeamTransmission as beam_transmission
-from pySC.lattice_properties.response_model import SCgetModelRM as model_rm, SCgetModelDispersion as model_dispersion, \
-    SCgetModelRING as model_ring
-
-from pySC.correction.loco_lib import SClocoLib as loco_lib
-from pySC.plotting.SCplotLattice import SCplotLattice as plot_lattice
-from pySC.plotting.SCplotPhaseSpace import SCplotPhaseSpace as plot_phase_space
-from pySC.plotting.SCplotSupport import SCplotSupport as plot_support
-from pySC.plotting.SCplotCMstrengths import SCplotCMstrengths as plot_cm_strength
+from pySC.core.classes import SimulatedComissioning
 from pySC.core.lattice_setting import SCsetCavs2SetPoints as cavity_setpoints, SCsetMags2SetPoints as magnet_setpoints, \
     SCsetCMs2SetPoints as cm_setpoints, SCsetMultipoles as multipole_setpoints, SCgetCMSetPoints as get_cm_setpoints, \
     SCcronoff as cronoff
+from pySC.correction.injection_fit import SCfitInjectionZ as fit_injection
+from pySC.correction.loco_lib import SClocoLib as loco_lib
+from pySC.correction.orbit_trajectory import SCfeedbackFirstTurn as first_turn, SCfeedbackStitch as stitch, \
+    SCfeedbackRun as frun, SCfeedbackBalance as fbalance, SCpseudoBBA as pseudo_bba
+from pySC.correction.ramp_errors import SCrampUpErrors as ramp_up_errors
 from pySC.correction.rf import SCsynchPhaseCorrection as synch_phase_corr, SCsynchEnergyCorrection as synch_energy_corr
+from pySC.correction.tune import SCtuneScan as tune_scan
 from pySC.lattice_properties.apertures import SCdynamicAperture as dynamic_aperture, \
     SCmomentumAperture as momentum_aperture
-from pySC.correction.bba import SCBBA as bba
+# from pySC.correction.bba import SCBBA as bba
 from pySC.lattice_properties.magnet_orbit import SCgetCOD as cod
 from pySC.lattice_properties.response_measurement import SCgetRespMat as response_matrix, SCgetDispersion as dispersion
-from pySC.correction.ramp_errors import SCrampUpErrors as ramp_up_errors
-from pySC.correction.tune import SCtuneScan as tune_scan
-from pySC.core.classes import SimulatedComissioning
+from pySC.lattice_properties.response_model import SCgetModelRM as model_rm, SCgetModelDispersion as model_dispersion, \
+    SCgetModelRING as model_ring
+from pySC.plotting.SCplotCMstrengths import SCplotCMstrengths as plot_cm_strength
+from pySC.plotting.SCplotLattice import SCplotLattice as plot_lattice
+from pySC.plotting.SCplotPhaseSpace import SCplotPhaseSpace as plot_phase_space
+from pySC.plotting.SCplotSupport import SCplotSupport as plot_support
+from pySC.utils import logging_tools
 from pySC.utils.sc_tools import SCgetOrds as get_ords, SCgetPinv as get_pinv, SCrandnc as randnc, \
     SCscaleCircumference as scale_circumference, SCgetTransformation as transform, SCmultipolesRead as read_multipoles
-from pySC.utils import logging_tools
 
 LOGGER = logging_tools.get_logger(__name__)
 LOGGER.warn("Matlab_index imported: "
@@ -53,6 +54,7 @@ def SCapplyErrors(SC: SimulatedComissioning, nsigmas: float = 2) -> SimulatedCom
 
 
 def SCBBA(SC, BPMords, magOrds, **kwargs):
+    return NotImplementedError
     return bba(SC, BPMords, magOrds, **kwargs)
 
 
@@ -64,7 +66,7 @@ def SCdynamicAperture(RING, dE, /, *, bounds=np.array([0, 1e-3]), nturns=1000, t
                       accuracy=1e-6, launchOnOrbit=False, centerOnOrbit=True, useOrbit6=False, auto=0, plot=False,
                       verbose=False):
     return dynamic_aperture(RING, dE, bounds=bounds, nturns=nturns, thetas=thetas, accuracy=accuracy,
-                      launchOnOrbit=launchOnOrbit, centerOnOrbit=centerOnOrbit, useOrbit6=useOrbit6, auto=auto,
+                            launchOnOrbit=launchOnOrbit, centerOnOrbit=centerOnOrbit, useOrbit6=useOrbit6, auto=auto,
                             plot=plot, verbose=verbose)
 
 
@@ -74,7 +76,8 @@ def SCfeedbackBalance(SC, Mplus, /, *, R0=None, CMords=None, BPMords=None, eps=1
 
 def SCfeedbackFirstTurn(SC, Mplus, /, *, R0=None, CMords=None, BPMords=None, maxsteps=100, wiggleAfter=20,
                         wiggleSteps=32, wiggleRange=np.array([500E-6, 1000E-6]), verbose=False):
-    return first_turn(SC, Mplus, reference=R0, CMords=CMords, BPMords=BPMords, maxsteps=maxsteps, wiggle_after=wiggleAfter,
+    return first_turn(SC, Mplus, reference=R0, CMords=CMords, BPMords=BPMords, maxsteps=maxsteps,
+                      wiggle_after=wiggleAfter,
                       wiggle_steps=wiggleSteps, wiggle_range=wiggleRange)
 
 
@@ -84,13 +87,16 @@ def SCfeedbackRun(SC, Mplus, /, *, R0=None, CMords=None, BPMords=None, eps=1e-4,
                 scaleDisp=scaleDisp)
 
 
-def SCfeedbackStitch(SC, Mplus, /, *, R0=None, CMords=None, BPMords=None, nBPMs=4, maxsteps=30, nRepro=3, wiggle_steps=32,
+def SCfeedbackStitch(SC, Mplus, /, *, R0=None, CMords=None, BPMords=None, nBPMs=4, maxsteps=30, nRepro=3,
+                     wiggle_steps=32,
                      wiggle_range=np.array([500E-6, 1000E-6])):
-    return stitch(SC, Mplus, reference=R0, CMords=CMords, BPMords=BPMords, nBPMs=nBPMs, maxsteps=maxsteps, nRepro=nRepro,
+    return stitch(SC, Mplus, reference=R0, CMords=CMords, BPMords=BPMords, nBPMs=nBPMs, maxsteps=maxsteps,
+                  nRepro=nRepro,
                   wiggle_steps=wiggle_steps, wiggle_range=wiggle_range)
 
 
-def SCfitInjectionZ(SC, mode, /, *, nDims=np.array([0,1]), nBPMs=np.array([0,1,2]), nShots=None, verbose=0, plotFlag=False):
+def SCfitInjectionZ(SC, mode, /, *, nDims=np.array([0, 1]), nBPMs=np.array([0, 1, 2]), nShots=None, verbose=0,
+                    plotFlag=False):
     return fit_injection(SC, mode, nDims=nDims, nBPMs=nBPMs, nShots=nShots, verbose=verbose, plotFlag=plotFlag)
 
 
@@ -129,7 +135,7 @@ def SCgetModelDispersion(SC, BPMords, CAVords, /, *, trackMode='ORB', Z0=np.zero
                             useIdealRing=useIdealRing)
 
 
-def SCgetModelRING(SC: SimulatedComissioning, /, *, includeAperture: bool =False) -> Lattice:
+def SCgetModelRING(SC: SimulatedComissioning, /, *, includeAperture: bool = False) -> Lattice:
     return model_ring(SC, includeAperture=includeAperture)
 
 
@@ -173,7 +179,8 @@ def SClocoLib(funName, *args):
 
 
 def SCmomentumAperture(RING, REFPTS, inibounds, /, *, nturns=1000, accuracy=1e-4, stepsize=1e-3, plot=0, debug=0):
-    return momentum_aperture(RING, REFPTS, inibounds, nturns=nturns, accuracy=accuracy, stepsize=stepsize, plot=plot, debug=debug)
+    return momentum_aperture(RING, REFPTS, inibounds, nturns=nturns, accuracy=accuracy, stepsize=stepsize, plot=plot,
+                             debug=debug)
 
 
 def SCmultipolesRead(fname):
@@ -196,7 +203,8 @@ def SCplotCMstrengths(SC: SimulatedComissioning):
     plot_cm_strength(SC)
 
 
-def SCplotLattice(SC, /, *, transferLine=0, nSectors=1, oList=[], plotIdealRing=1, sRange=[], plotMagNames=0, fontSize=16):
+def SCplotLattice(SC, /, *, transferLine=0, nSectors=1, oList=[], plotIdealRing=1, sRange=[], plotMagNames=0,
+                  fontSize=16):
     return plot_lattice(SC, transferLine=transferLine, nSectors=nSectors, oList=oList, plotIdealRing=plotIdealRing,
                         sRange=sRange, plotMagNames=plotMagNames, fontSize=fontSize)
 
@@ -214,10 +222,11 @@ def SCpseudoBBA(SC, BPMords, MagOrds, postBBAoffset, /, *, sigma=2):
 
 
 def SCrampUpErrors(SC, /, *, nStepsRamp=10, eps=1e-5, target=0, alpha=10, maxsteps=30, verbose=0):
-    return ramp_up_errors(SC, nStepsRamp=nStepsRamp, eps=eps, target=target, alpha=alpha, maxsteps=maxsteps, verbose=verbose)
+    return ramp_up_errors(SC, nStepsRamp=nStepsRamp, eps=eps, target=target, alpha=alpha, maxsteps=maxsteps,
+                          verbose=verbose)
 
 
-def SCrandnc(cut_off: float = 2, shape: tuple = (1, )) -> ndarray:
+def SCrandnc(cut_off: float = 2, shape: tuple = (1,)) -> ndarray:
     return randnc(cut_off, shape)
 
 
@@ -236,7 +245,8 @@ def SCregisterMagnets(SC: SimulatedComissioning, MAGords: ndarray, **kwargs) -> 
     return SC
 
 
-def SCregisterSupport(SC: SimulatedComissioning, support_type: str, support_ords: ndarray, **kwargs) -> SimulatedComissioning:
+def SCregisterSupport(SC: SimulatedComissioning, support_type: str, support_ords: ndarray,
+                      **kwargs) -> SimulatedComissioning:
     SC.register_supports(support_ords=support_ords, support_type=support_type, **kwargs)
     return SC
 
@@ -245,7 +255,7 @@ def SCSanityCheck(SC: SimulatedComissioning) -> None:
     SC.verify_structure()
 
 
-def SCscaleCircumference(RING, circ,  /, *, mode='abs'):
+def SCscaleCircumference(RING, circ, /, *, mode='abs'):
     return scale_circumference(RING, circ, mode=mode)
 
 
@@ -271,11 +281,11 @@ def SCsetMags2SetPoints(SC: SimulatedComissioning, MAGords: ndarray, type: int, 
     if type not in (1, 2):
         raise ValueError("Function expects type 1 (skew) or 2 (normal)")
 
-    return magnet_setpoints(SC, MAGords=MAGords,  skewness=(type == 1), order=order-1, setpoints=setpoints,
+    return magnet_setpoints(SC, MAGords=MAGords, skewness=(type == 1), order=order - 1, setpoints=setpoints,
                             method=method, dipCompensation=dipCompensation)
 
 
-def SCsetMultipoles(RING, ords: ndarray, AB,  /, *, method: str = 'rnd', order: int = None, type: int = None):
+def SCsetMultipoles(RING, ords: ndarray, AB, /, *, method: str = 'rnd', order: int = None, type: int = None):
     LOGGER.warn("Function SCsetMultipoles contains non-trivial transition between Matlab and Python code.\n"
                 "Transition concerns:    type 1 -> skew, 2-> normal. \n"
                 "                        order 1 -> dipole, 2-> quadrupole, 3-> sextupole, ...\n"
@@ -286,21 +296,23 @@ def SCsetMultipoles(RING, ords: ndarray, AB,  /, *, method: str = 'rnd', order: 
         if type not in (1, 2):
             raise ValueError("Function expects type 1 (skew) or 2 (normal)")
     return multipole_setpoints(RING=RING, ords=ords, BA=np.roll(AB, 1, axis=1), method=method,
-                               order=None if order is None else order-1,
+                               order=None if order is None else order - 1,
                                skewness=None if order is None else (type == 1))
 
 
-def SCsynchEnergyCorrection(SC,  /, *, cavOrd=None, f_range=(-1E3, 1E3), nSteps=15, nTurns=150, minTurns=0, plotResults=False,
-                                    plotProgress=False, verbose=False):
+def SCsynchEnergyCorrection(SC, /, *, cavOrd=None, f_range=(-1E3, 1E3), nSteps=15, nTurns=150, minTurns=0,
+                            plotResults=False,
+                            plotProgress=False, verbose=False):
     init_nturns = SC.INJ.nTurns
     SC.INJ.nTurns = nTurns
     SC = synch_energy_corr(SC, cavOrd=cavOrd, f_range=f_range, nSteps=nSteps, minTurns=minTurns,
-                             plotResults=plotResults, plotProgress=plotProgress, )
+                           plotResults=plotResults, plotProgress=plotProgress, )
     SC.INJ.nTurns = init_nturns
     return SC
 
 
-def SCsynchPhaseCorrection(SC,  /, *, cavOrd=None, nSteps=15, nTurns=20, plotResults=False, plotProgress=False, verbose=False):
+def SCsynchPhaseCorrection(SC, /, *, cavOrd=None, nSteps=15, nTurns=20, plotResults=False, plotProgress=False,
+                           verbose=False):
     init_nturns = SC.INJ.nTurns
     SC.INJ.nTurns = nTurns
     SC = synch_phase_corr(SC, cavOrd=cavOrd, nSteps=nSteps, plotResults=plotResults, plotProgress=plotProgress, )
@@ -308,7 +320,8 @@ def SCsynchPhaseCorrection(SC,  /, *, cavOrd=None, nSteps=15, nTurns=20, plotRes
     return SC
 
 
-def SCtuneScan(SC, qOrds, qSPvec, /, *, verbose=False, plotFlag=False, nParticles=None, nTurns=None, target=1, fullScan=0):
+def SCtuneScan(SC, qOrds, qSPvec, /, *, verbose=False, plotFlag=False, nParticles=None, nTurns=None, target=1,
+               fullScan=0):
     return tune_scan(SC, qOrds, qSPvec, verbose=verbose, plotFlag=plotFlag, nParticles=nParticles, nTurns=nTurns,
                      target=target, fullScan=fullScan)
 
@@ -323,6 +336,7 @@ def SCupdateMagnets(SC: SimulatedComissioning, ords: ndarray = None) -> Simulate
     return SC
 
 
-def SCupdateSupport(SC: SimulatedComissioning, BPMstructOffset: bool = True, MAGstructOffset: bool = True) -> SimulatedComissioning:
+def SCupdateSupport(SC: SimulatedComissioning, BPMstructOffset: bool = True,
+                    MAGstructOffset: bool = True) -> SimulatedComissioning:
     SC.update_supports(offset_bpms=BPMstructOffset, offset_magnets=MAGstructOffset)
     return SC
