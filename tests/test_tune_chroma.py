9e349c599d9508b3599ffb51f0913d2e119c229b
Lina Hoummi <hoummi@raki3.esrf.fr>
2023-10-11 11:16:34 +0200

    added the assess line

--- tests/test_tune_chroma.py
+++ tests/test_tune_chroma.py
@@ -1,29 +1,19 @@
-import copy
-import at
-import pytest
 import numpy as np
-from numpy.testing import assert_equal, assert_allclose
+from numpy.testing import assert_allclose
 from tests.test_at_wrapper import at_lattice
-from pySC.correction.tune import tune_scan, fit_tune
-from pySC.correction.chroma import fit_chroma
 from pySC.core.simulated_commissioning import SimulatedCommissioning
+from pySC.correction.tune import fit_tune
 from pySC.utils.sc_tools import SCgetOrds
 
 
 def test_fit_tune(at_lattice):
-    sc = SimulatedCommissioning(at_lattice)
-    sc.register_cavities(SCgetOrds(sc.RING, 'RFCav'),
-                         FrequencyOffset=5E3,
-                         VoltageOffset=5E3,
-                         TimeLagOffset=0.5)
-    sc.register_magnets(SCgetOrds(sc.RING, 'QF'), HCM=1E-3,
-                        CalErrorB=np.array([5E-2, 1E-3]),
-                        MagnetOffset=200E-6 * np.array([1, 1, 0]),
-                        MagnetRoll=200E-6 * np.array([1, 0, 0]))
-    sc.register_magnets(SCgetOrds(sc.RING, 'QD'), VCM=1E-3,
-                        CalErrorA=np.array([5E-2, 0]),
-                        CalErrorB=np.array([0, 1E-3]),
-                        MagnetOffset=200E-6 * np.array([1, 1, 0]),
-                        MagnetRoll=200E-6 * np.array([1, 0, 0]))
-    sc = fit_tune(sc, q_ords=[SCgetOrds(sc.RING, 'QF'), SCgetOrds(sc.RING, 'QD')], target_tune=np.array([0.16, 0.21]))
+    SC = sc(at_lattice)
+    SC = fit_tune(SC, q_ords=[SCgetOrds(SC.RING, 'QF'), SCgetOrds(SC.RING, 'QD')], target_tune=SC.RING.get_tune()[:2]+[0.005,-0.005])
+    assert_allclose(actual=SC.RING.get_tune()[:2], desired=SC.RING.get_tune()[:2]+[0.1,0.1], rtol=1e-2)
     return sc
+
+def sc(at_lattice):
+    SC = SimulatedCommissioning(at_lattice)
+    SC.register_magnets(SCgetOrds(SC.RING, 'QF'))
+    SC.register_magnets(SCgetOrds(SC.RING, 'QD'))
+    return SC
